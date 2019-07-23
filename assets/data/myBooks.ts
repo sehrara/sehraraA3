68a675c415d138cf0ed7a7fcfca822cf5fa93f74
sehrara <52628391+sehrara@users.noreply.github.com>
2019-07-23 10:53:58 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,10 @@
+import {Books} from '../../app/booksInfo';
+
+export const MYCLASSES: Books[] = [
+
+    {bName: 'EDUCATED', bAuthor: 'Tara Westover', bGenre: 'Western', rYearPublished: 'February 20, 2018', rPicture: 'Yes', blink: '../../assets/images/Educated.JPG'},
+    {bName: 'THE GERMAN MIDWIFE', bAuthor: 'Mandy Robotham', bGenre: 'Mystery', rYearPublished: 'June 18, 2019', rPicture: 'Yes', blink: '../../assets/images/GermanWife.JPG'},
+    {bName: 'THE ONE: A NOVEL', bAuthor: 'John Marrs', bGenre: 'Thriller', rYearPublished: 'April 16, 2019', rPicture: 'Yes', blink: '../../assets/images/TheOne.JPG'},
+    {bName: 'THE HUNTRESS: A NOVEL', bAuthor: 'Kate Quinn', bGenre: 'Thriller', rYearPublished: 'February 26, 2019', rPicture: 'Yes', blink: '../../assets/images/TheHuntress.JPG'}
+
+] 
